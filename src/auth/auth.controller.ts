58e5aaa9ecc3aa48e77f746c11e401c280557002
Toe Wai Phyo <toewaiphyo@i.viabells.com>
2024-11-02 14:52:39 +0700

    authentication routes

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/signup.dto';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/register')
+  async register(@Body() signUpDto: SignUpDto): Promise<{ token: string }> {
+    return this.authService.signUp(signUpDto);
+  }
+  @Post('/login')
+  async login(@Body() loginDto: LoginDto): Promise<{ token: string }> {
+    return this.authService.login(loginDto);
+  }
+}
